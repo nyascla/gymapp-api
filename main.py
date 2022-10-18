ee0b299d8a97ea6f70a02c71d40864bec1686c24
nyascla <llorenslavernia123@gmail.com>
2022-10-18 18:00:20 +0200

    Cositas

--- main.py
+++ main.py
@@ -1,16 +1,60 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-#comentario 
+# root
+@app.get("/")
+async def serve_spa(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/api")
+# plantilla entreno
+@app.get("/api/workout")
 def read_root():
-    return {"Hello": "World"}
+    return {
+        "pb": {
+            "s": 1,
+            "r": 1,
+            "p": 1
+        },
+        "rB": {
+            "s": 2,
+            "r": 2,
+            "p": 2
+        },
+        "jalon": {
+            "s": 3,
+            "r": 3,
+            "p": 3
+        }
+    }
 
+# plantilla comida
+@app.get("/api/foot")
+def read_root():
+    return {
+        "Comida": "World",
+        "Gym": "World",
+        "Otros": "World"
+    }
+# plantilla medidas
+@app.get("/api/size")
+def read_root():
+    return {
+        "Comida": "World",
+        "Gym": "World",
+        "Otros": "World"
+    }
 
-@app.get("/items/{item_id}")
+@app.get("/api/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+# DEV
+# uvicorn main:app --reload
