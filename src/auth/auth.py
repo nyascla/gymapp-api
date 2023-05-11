dfa8f60c79ee4a03d0d9ae2791295347b3159d81
nyascla <llorenslavernia123@gmail.com>
2023-05-11 16:46:16 +0200

    requirements.txt

---
+++ src/auth/auth.py
@@ -0,0 +1,7 @@
+import jwt
+
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from passlib.context import CryptContext
+
+from datetime import datetime, timedelta
