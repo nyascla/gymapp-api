29743468e1dbb969eb5848fadc1019b2696b8b9f
nyascla <lavernia.llorens@gmail.com>
2024-11-18 23:14:44 +0100

    mejoras

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from datetime import datetime
+import os
 
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.middleware.cors import CORSMiddleware
 
-from src.routes import exercise
 from src.routes import auth
-
+from src.routes import exercise
 from src.sqlite import models
 from src.sqlite.database import engine, triggers, inserts
 
@@ -27,13 +26,13 @@ app.add_middleware(
     allow_headers=["*"],  # Permitir todos los headers
 )
 
+if not os.path.exists("./static"):
+    os.makedirs("./static")
+
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 
 @app.get("/")
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-
--- src/sqlite/database.py
+++ src/sqlite/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import text
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./DB/sql_database.db"
 
@@ -12,6 +13,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+
 def get_sqlite():
     db = SessionLocal()
     try:
@@ -19,39 +21,46 @@ def get_sqlite():
     finally:
         db.close()
 
+
 def triggers(_engine):
+    trigger_sql = """
+    CREATE TRIGGER set_number_increment
+    AFTER INSERT ON sets
+    FOR EACH ROW
+    BEGIN
+        UPDATE sets
+        SET set_number = (SELECT COALESCE(MAX(set_number), 0) + 1 FROM sets WHERE exercise_session_id = NEW.exercise_session_id)
+        WHERE id = NEW.id;
+    END;
+    """
     try:
         with _engine.connect() as conn:
-            conn.execute("""
-            CREATE TRIGGER set_number_increment
-            AFTER INSERT ON sets
-            FOR EACH ROW
-            BEGIN
-                UPDATE sets
-                SET set_number = (SELECT COALESCE(MAX(set_number), 0) + 1 FROM sets WHERE exercise_session_id = NEW.exercise_session_id)
-                WHERE id = NEW.id;
-            END;
-            """)
-    except:
+            conn.execute(text(trigger_sql))
+    except Exception as e:
         pass
 
+
 def inserts(_engine):
+    insert_patterns = """
+    INSERT INTO patterns (name)
+    VALUES ("Push"),
+        ("Pull"),
+        ("Legs");
+    """
+
+    insert_exercises = """
+    INSERT INTO exercises (name, pattern)
+    VALUES ("Bench Press", "Push"),
+         ("Squad", "Legs"),
+         ("Pull-Ups", "Pull"),
+         ("Shoulder Pres", "Push");
+    """
     try:
         with _engine.connect() as conn:
-            conn.execute("""
-                INSERT INTO patterns (name)
-                VALUES ("Push"),
-                    ("Pull"),
-                    ("Legs");
-            """)
+            conn.execute(text(insert_patterns))
 
         with _engine.connect() as conn:
-            conn.execute("""
-                INSERT INTO exercises (name, pattern)
-                VALUES ("Bench Press", "Push"),
-                     ("Squad", "Legs"),
-                     ("Pull-Ups", "Pull"),
-                     ("Shoulder Pres", "Push");
-            """)
+            conn.execute(text(insert_exercises))
+
     except Exception as e:
         pass
